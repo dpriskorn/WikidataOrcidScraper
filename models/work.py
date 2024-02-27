9d52484cba3ac8afd7fb94c99a5cdf93869990dc
Dennis Priskorn <egils@riseup.net>
2024-02-27 18:03:47 +0100

    fix: Fix DOI lookup and goto links.

--- models/work.py
+++ models/work.py
@@ -14,6 +14,16 @@ class Work(BaseModel):
     doi: str
     # session: Session = requests.session()
 
+    # query = 'select ?work where {{ ?work wdt:P356 "{doi}" }}'.format(
+    #     doi=escape_string(doi.upper()))
+
+    @staticmethod
+    def escape_string(string: str):
+        """This prepares the string for sparql
+        Borrowed from Scholia at
+        https://github.com/WDscholia/scholia/blob/058cf03b76eb45548928a169fe063586ce9db6de/scholia/query.py#L161C5-L161C60"""
+        return string.replace('\\', '\\\\').replace('"', r'\"')
+
     @property
     def lookup_qid_using_qlever(self):
         logger.debug("lookup_qid_using_qlever: running")
@@ -22,19 +32,19 @@ class Work(BaseModel):
             SELECT ?item
             WHERE
             {{
-              ?item wdt:P356 "{self.doi}".
+              ?item wdt:P356 "{self.escape_string(string=self.doi.upper())}".
             }}
         """
         qi = QleverIntegrator()
         result = qi.execute_qlever_sparql_query(query=query)
-        pprint(result)
+        # pprint(result)
         # empty result
         # {'head': {'vars': ['item']}, 'results': {'bindings': []}}
         bindings = result.get("results").get("bindings")
         if bindings:
             first_binding = bindings[0]
             # pprint(first_binding)
-            qid = first_binding.get("item").get("value")
+            qid = first_binding.get("item").get("value")[31:]
             logger.debug(f"qid found: {qid}")
             return qid
         else:
@@ -44,7 +54,7 @@ class Work(BaseModel):
     def row_html(self):
         """Lookup and return row"""
         qid = self.lookup_qid_using_qlever
-        if qid:
+        if not qid:
             qid_html = f"<td><a href='{self.scholia_link}'>Missing</a></td>"
         else:
             qid_html = f"<td><a href='{Item(qid=qid).url}'>{qid}</td>"
