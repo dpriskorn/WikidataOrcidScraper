7be6356d1324c0cae37119a3a34c24d110555007
Dennis Priskorn <egils@riseup.net>
2024-05-15 10:30:13 +0200

    feat: Lookup citations from opencitations also and display the count fixes #8 fixes #9

--- models/work.py
+++ models/work.py
@@ -1,7 +1,9 @@
 import logging
-from datetime import datetime
-from pprint import pprint
+import timeit
+from datetime import date
+from urllib.parse import quote
 
+import requests
 from pydantic import BaseModel, ConfigDict
 
 from models.item import Item
@@ -13,10 +15,12 @@ logger = logging.getLogger(__name__)
 class Work(BaseModel):
     title: str
     doi: str
-    date: datetime.date
+    date: date
     model_config = ConfigDict(  # dead:disable
         arbitrary_types_allowed=True, extra="forbid"
     )
+    citation_count: int | None = None
+    qid: str = ""
     # session: Session = requests.session()
 
     # query = 'select ?work where {{ ?work wdt:P356 "{doi}" }}'.format(
@@ -26,7 +30,8 @@ class Work(BaseModel):
     def escape_string(string: str):
         """This prepares the string for sparql
         Borrowed from Scholia at
-        https://github.com/WDscholia/scholia/blob/058cf03b76eb45548928a169fe063586ce9db6de/scholia/query.py#L161C5-L161C60
+        https://github.com/WDscholia/scholia/blob/
+        058cf03b76eb45548928a169fe063586ce9db6de/scholia/query.py#L161C5-L161C60
         """
         return string.replace("\\", "\\\\").replace('"', r"\"")
 
@@ -50,9 +55,9 @@ class Work(BaseModel):
         if bindings:
             first_binding = bindings[0]
             # pprint(first_binding)
-            qid = first_binding.get("item").get("value")[31:]
-            logger.debug(f"qid found: {qid}")
-            return qid
+            self.qid = first_binding.get("item").get("value")[31:]
+            logger.debug(f"qid found: {self.qid}")
+            return self.qid
         else:
             # logger.debug(f"query: {query}")
             # exit()
@@ -61,16 +66,27 @@ class Work(BaseModel):
     @property
     def row_html(self):
         """Lookup and return row"""
+        self.fetch_citation_count()  # Fetch citation count
         qid = self.lookup_qid_using_qlever
-        if not qid:
-            qid_html = f"<td><a href='{self.scholia_link}'>Missing</a></td>"
-        else:
-            qid_html = f"<td><a href='{Item(qid=qid).url}' target='_blank'>{qid}</td>"
+        qid_html = (
+            f"<td><a href='{self.scholia_link}'>Missing</a></td>"
+            if not qid
+            else f"<td><a href='{Item(qid=qid).url}' target='_blank'>{qid}</td>"
+        )
+        citation_link = (
+            f"https://opencitations.net/index/search?text={self.qouted_doi}&rule=citeddoi"
+        )
+        citation_count_html = (
+            f'<td><a href="{citation_link}" target="_blank">{self.citation_count}</a></td>'
+            if self.citation_count is not None
+            else "<td>N/A</td>"
+        )
         return f"""
         <tr>
             <td>{self.date}</td>
             <td>{self.title}</td>
             <td><a href="{self.doi_link}" target="_blank">{self.doi}</a></td>
+            {citation_count_html}
             {qid_html}
         </tr>
         """
@@ -82,3 +98,32 @@ class Work(BaseModel):
     @property
     def doi_link(self):
         return f"https://doi.org/{self.doi}"
+
+    def fetch_citation_count(self):
+        logger.debug("Fetching citation count from OpenCitations")
+        start_time = timeit.default_timer()  # Start timing
+        endpoint = (
+            f"https://opencitations.net/index/"
+            f"api/v2/citation-count/doi:{self.qouted_doi}"
+        )
+        try:
+            response = requests.get(endpoint)
+            response.raise_for_status()
+            citation_data = response.json()
+            if (
+                citation_data
+                and isinstance(citation_data, list)
+                and "count" in citation_data[0]
+            ):
+                self.citation_count = int(citation_data[0]["count"])
+            else:
+                # We get empty list if no result is found and do nothing
+                pass
+        except requests.exceptions.RequestException as e:
+            logger.error(f"Error fetching citation count: {e}")
+        elapsed_time = timeit.default_timer() - start_time  # Calculate elapsed time
+        logger.debug(f"Time taken to fetch citation count: {elapsed_time} seconds")
+
+    @property
+    def qouted_doi(self):
+        return quote(self.doi)
